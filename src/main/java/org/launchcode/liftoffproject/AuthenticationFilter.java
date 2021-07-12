475014927fddec9183a44d0ff31f85622a2e70a5
amy sanders <amysanders89@gmail.com>
2021-07-11 20:45:59 -0500

    added auth filter completed user authentication

---
+++ src/main/java/org/launchcode/liftoffproject/AuthenticationFilter.java
@@ -0,0 +1,54 @@
+package org.launchcode.liftoffproject;
+
+import org.launchcode.liftoffproject.controllers.AuthenticationController;
+import org.launchcode.liftoffproject.data.UserRepository;
+import org.launchcode.liftoffproject.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class AuthenticationFilter extends HandlerInterceptorAdapter {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    AuthenticationController authenticationController;
+
+    private static final List<String> allowedList = Arrays.asList("/login", "/register", "/logout", "/css", "/");
+
+    private static boolean isAllowed(String path) {
+        for (String pathRoot : allowedList) {
+            if (path.equals(pathRoot)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler) throws IOException {
+
+        if (isAllowed(request.getRequestURI())) {
+            return true;
+        }
+
+        HttpSession session = request.getSession();
+        User user = authenticationController.getUserFromSession(session);
+
+        if (user != null) {
+            return true;
+        }
+
+        response.sendRedirect("/login");
+        return false;
+    }
+}
